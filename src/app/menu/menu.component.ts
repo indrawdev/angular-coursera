dcafd64f7ff3069afd39480037cd3c14e0a26cbe
indrapramana <idewebid@gmail.com>
2021-04-02 14:45:59 +0700

    Assigment 1

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 
-@Component({
-	selector: 'app-menu',
-	templateUrl: './menu.component.html',
-	styleUrls: ['./menu.component.scss']
-})
-
 const DISHES: Dish[] = [
 	{
 		id: '0',
@@ -50,13 +44,19 @@ const DISHES: Dish[] = [
 	}
 ];
 
+@Component({
+	selector: 'app-menu',
+	templateUrl: './menu.component.html',
+	styleUrls: ['./menu.component.scss']
+})
+	
 export class MenuComponent implements OnInit {
-
+	
   dishes = DISHES;
 
   selectedDish = DISHES[0];
 
-	constructor() { }
+  constructor() { }
 
 	ngOnInit() {
 	}
