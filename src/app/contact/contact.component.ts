e96453eb55053dc1bf195971b50e5e6ec9ed8c36
Indra <indra@ide.web.id>
2021-04-13 15:16:07 +0700

    Reactive Forms Part 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Feedback, ContactType } from  '../shared/feeback'
+import { Feedback, ContactType } from '../shared/feeback'
 
 @Component({
   selector: 'app-contact',
@@ -8,6 +8,8 @@ import { Feedback, ContactType } from  '../shared/feeback'
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+
+	@ViewChild('fform') feedbackFormDirective;
 
 	feedbackForm: FormGroup;
   feedback: Feedback;
@@ -20,22 +22,31 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
 	}
 	
-	createForm() {
+  createForm() {
     this.feedbackForm = this.fb.group({
+      firstname: ['', Validators.required ],
+      lastname: ['', Validators.required ],
+      telnum: ['', Validators.required ],
+      email: ['', Validators.required ],
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+  }
+	
+  onSubmit() {
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset({
       firstname: '',
       lastname: '',
-      telnum: 0,
+      telnum: '',
       email: '',
       agree: false,
       contacttype: 'None',
       message: ''
     });
-	}
-	
-	onSubmit() {
-    this.feedback = this.feedbackForm.value;
-    console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackFormDirective.resetForm();
   }
 
 }
