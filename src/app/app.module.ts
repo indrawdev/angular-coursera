8527d97963f48fd075b5ad0a429d0510caad8f13
Indra <indra@ide.web.id>
2021-04-13 14:54:19 +0700

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -21,12 +25,11 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 import { DishService } from './services/dish.service';
 import { LeaderService } from './services/leader.service';
 import { PromotionService } from './services/promotion.service';
-import { LoginComponent } from './login/login.component';
-
 
 @NgModule({
 	declarations: [
@@ -50,7 +53,11 @@ import { LoginComponent } from './login/login.component';
 		MatCardModule,
 		MatButtonModule,
 		MatDialogModule,
-		AppRoutingModule
+		MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+		AppRoutingModule,
+		FormsModule
 	],
 	providers: [DishService, LeaderService, PromotionService],
 	bootstrap: [AppComponent],
