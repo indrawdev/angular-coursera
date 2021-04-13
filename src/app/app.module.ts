4f853c25954c9cee215fcd96dbae82b8dceed3b0
Indra <indra@ide.web.id>
2021-04-13 14:41:56 +0700

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -15,37 +16,46 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
+import { DishService } from './services/dish.service';
+import { LeaderService } from './services/leader.service';
+import { PromotionService } from './services/promotion.service';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent,
-    DishdetailComponent,
-    HeaderComponent,
-    FooterComponent,
-    AboutComponent,
-    HomeComponent,
-    ContactComponent
-  ],
-  imports: [
+	declarations: [
+		AppComponent,
+		MenuComponent,
+		DishdetailComponent,
+		HeaderComponent,
+		FooterComponent,
+		AboutComponent,
+		HomeComponent,
+		ContactComponent,
+		LoginComponent
+	],
+	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
-    MatToolbarModule,
+		MatToolbarModule,
 		FlexLayoutModule,
 		MatListModule,
 		MatGridListModule,
-    MatCardModule,
+		MatCardModule,
 		MatButtonModule,
+		MatDialogModule,
 		AppRoutingModule
-  ],
-  providers: [DishService],
-  bootstrap: [AppComponent]
+	],
+	providers: [DishService, LeaderService, PromotionService],
+	bootstrap: [AppComponent],
+	entryComponents: [
+		LoginComponent
+	],
 })
 export class AppModule { }
